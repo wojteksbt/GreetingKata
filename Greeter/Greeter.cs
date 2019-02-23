837a673235ad5ad674d20c9825c14b998ee3a6d3
Wojciech Sabat <Wojciech Sabat>
2019-02-23 17:34:34 +0100

    Filled requirement 1

--- Greeter/Greeter.cs
+++ Greeter/Greeter.cs
@@ -2,9 +2,6 @@ namespace Greeter
 {
     public class Greeter : IGreeter
     {
-        public string Greet(string name)
-        {
-            throw new System.NotImplementedException();
-        }
+        public string Greet(string name) => $"Hello, {name}.";
     }
 }
