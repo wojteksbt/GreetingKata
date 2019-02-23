898c8556f344038e565549a333b58d3f4dd1bd63
Wojciech Sabat <Wojciech Sabat>
2019-02-23 17:41:50 +0100

    Filled requirement 2

--- Greeter/Greeter.cs
+++ Greeter/Greeter.cs
@@ -2,6 +2,13 @@ namespace Greeter
 {
     public class Greeter : IGreeter
     {
-        public string Greet(string name) => $"Hello, {name}.";
+        public string Greet(string name)
+        {
+            if (name == null)
+            {
+                return "Hello, my friend.";
+            }
+            return $"Hello, {name}.";
+        }
     }
 }
