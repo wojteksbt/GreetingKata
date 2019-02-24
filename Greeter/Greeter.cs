f656a276e441e22d493af4ada9ce4cbfca6a29f0
Wojciech Sabat <Wojciech Sabat>
2019-02-24 18:38:36 +0100

    Refactored after requirement 5

--- Greeter/Greeter.cs
+++ Greeter/Greeter.cs
@@ -10,24 +10,35 @@ namespace Greeter
         {
             if (names[0] == null)
                 return DefaultGreeting;
+
+            var greeted = FormatGreeted(names);
                 
-            var greeting = $"Hello, {BuildGreeted(names)}.";
+            var greeting = $"Hello, {greeted}.";
 
-            if (names.All(n => n.IsUpperCased()))
-                return Shout(greeting);
-            
-            return greeting;
+            return ShouldGreetingBeShouted(names) ? Shout(greeting) : greeting;
         }
 
-        private static string BuildGreeted(string[] names)
+        private static string FormatGreeted(string[] names)
         {
-            if (names.Length <= 2)
-                return string.Join(" and ", names);
-
-            var joinedWithoutLast = string.Join(", ", names, 0, names.Length - 1);
-
-            return $"{joinedWithoutLast}, and {names.Last()}";
+            switch (names.Length)
+            {
+                case 1:
+                    return names[0];
+                case 2:
+                    return $"{names[0]} and {names[1]}";
+                default:
+                    return FormatManyGreeted(names);
+            }
         }
+
+        private static string FormatManyGreeted(string[] names)
+        {
+            var formattedAllButLastElement = string.Join(", ", names, 0, names.Length - 1);
+
+            return $"{formattedAllButLastElement}, and {names.Last()}";
+        }
+        
+        private static bool ShouldGreetingBeShouted(string[] names) => names.All(n => n.IsUpperCased());
 
         private static string Shout(string text) => text.ToUpperInvariant();
     }
